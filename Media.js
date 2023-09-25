bde428cc3bd6c0f4bdf14619e4a8f08957339ddd
Vitorr88 <126670542+Vitorr88@users.noreply.github.com>
2023-09-25 11:29:46 -0300

    Add files via upload

--- Media.js
+++ Media.js
@@ -15,7 +15,8 @@ if(media >= 7) {
     resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
     resp2.style.color = "blue"
 } else if (media >=4) {
-    resp2.style.color = "grenn"  
+    resp2.innerText = `Atenção ${nome}! Você está de exame`
+    resp2.style.color = "green"  
 } else {
     resp2.innerText = `Ops ${nome}! Você foi reprovado(a)`
     resp2.style.color = "red"
