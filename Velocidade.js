bb84dd782c1cf9c52b1a270a297b4f20422d0e5e
Vitorr88 <126670542+Vitorr88@users.noreply.github.com>
2023-10-16 11:45:21 -0300

    Add files via upload

--- Velocidade.js
+++ Velocidade.js
@@ -1,8 +1,20 @@
 const form = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-form.addEventlitener("submit",(e)=> {
+form.addEventListener("submit",(e) =>{
     e.preventDefault()
 
-    const 
+    const permitida = Number(form.inPermitida.value)
+    const condutor = Number(form.inCondutor.value)
+    let leve = permitida + (permitida * 0.2)
+
+    if(condutor <= permitida){
+        resp.innerText = `Situação: Sem Multa`
+
+    } else if(condutor <= leve) {
+        resp.innerText = `Situação: Multa Leve`
+
+    } else {
+        resp.innerText = `Situação: Multa grave`
+    }
 })
